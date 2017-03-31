10287d4820ff597060c1ca2068463ecbecc1af99
Brandon <loxmythart@gmail.com>
2017-03-31 10:06:21 -0700

    added depth based on y value

---
+++ Assets/scripts/DepthByHeight.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DepthByHeight : MonoBehaviour {
+
+
+	protected Vector3 pos;
+	protected float depth;
+
+	// Use this for initialization
+	void Start () 
+	{
+		pos = this.transform.position;
+		depth = pos.y;
+		pos.z = depth;
+
+		this.transform.position = pos;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
